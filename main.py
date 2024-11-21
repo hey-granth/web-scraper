4c05bdb1290729b7dd54536af7859e789283b3a2
Granth Agarwal <heygranth@gmail.com>
2024-11-21 09:47:47 +0530

    clean DOM content

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import streamlit as st
+from scrape import scrape_website, split_dom_content, clean_body_content, extract_body_content
 
 st.title('AI Web Scraper')
 url = st.text_input('Enter a website URL:')
 
 if st.button("Scrape Site"):
     st.write(f"Scraping {url}...")
-    # Scrape the website here
-    st.write("Scraping complete!")
+    result = scrape_website(url)
+    body_content = extract_body_content(result)
+    clean_content = clean_body_content(body_content)
+    st.session_state.dom_content = clean_content
+
+    with st.expander("View DOM content"):
+        st.text_area("DOM content", value=clean_content, height=300)
