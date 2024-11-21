ea7e91822a12e89451231cbbffc697235f4ec0cb
Granth Agarwal <heygranth@gmail.com>
2024-11-21 19:29:49 +0530

    added parsing config

--- main.py
+++ main.py
@@ -13,3 +13,10 @@ if st.button("Scrape Site"):
 
     with st.expander("View DOM content"):
         st.text_area("DOM content", value=clean_content, height=300)
+
+
+if "dom_content" in st.session_state:
+    parse_description = st.text_area('Describe what you wanna parse?')
+    if st.button("Parse Content"):
+        st.write(f"Parsing {parse_description}...")
+        dom_chunks = split_dom_content(st.session_state.dom_content)
